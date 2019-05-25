96206b27a48468408ab8c6d0aff839b4acbc32fc
Adnan Hasic <hasic.adnan.ado@gmail.com>
2019-05-25 22:02:12 +0200

    kreiran test metod za setovanje vrijednosti duzine na 1 ukoliko je unesena vrijednost <= 0

--- src/ConverterUnitTest.java
+++ src/ConverterUnitTest.java
@@ -6,11 +6,13 @@ import org.junit.Test;
 public class ConverterUnitTest {
 
 	ConverterUnit c1;
+	ConverterUnit c2;
 	double delta;
 	
 	@Before
 	public void setUp() {
 		c1 = new ConverterUnit(10.0);
+		c2 = new ConverterUnit();
 		delta = 0.01;
 	}
 	
@@ -19,6 +21,15 @@ public class ConverterUnitTest {
 		
 		assertEquals(10.0, c1.getValueOfLength(), delta);
 		
+	}
+	
+	@Test
+	public void valueOfLengthIsLessThanOrEqualToZeroTest_ShouldSetValueOfLengthOnOne_IfValueOfLengthIsLessThanOrEqualToZero() {
+	     
+		c2 = new ConverterUnit(0);
+		c2.valueOfLengthIsLessThanOrEqualToZero();
+		
+		assertEquals(1, c2.getValueOfLength(), delta);
 	}
 	
 
